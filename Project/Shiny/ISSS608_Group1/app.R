95c4bf528d5a58d828ed6a77ad4f235c154f0d73
brigittatsai <brigittakaren@gmail.com>
2025-03-21 15:03:46 +0800

    project code

--- Project/Shiny/ISSS608_Group1/app.R
+++ Project/Shiny/ISSS608_Group1/app.R
@@ -1,51 +1,50 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
+pacman::p_load(shiny, tidyverse, rsconnect)
 library(shiny)
+library(tidyverse)
+library(rsconnect)
+library(readxl)
+library(data.table)
 
-# Define UI for application that draws a histogram
+folder_path <- "ISSS608_Group1/data/aspatial/weather"
+file_list <- list.files(path = folder_path, pattern = "\\.csv$", full.names = TRUE)
+
+# Read and combine all Excel files using data.table
+combined_data <- rbindlist(lapply(file_list, fread))
+
+# View the merged dataframe
+head(combined_data)
+
+
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+  titlePanel("Pupil Exam Results Dashboard"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(inputId = "variable",
+                  label = "Subject:",
+                  choices = c("English" = "ENGLISH",
+                              "Maths" = "MATHS",
+                              "Science" = "SCIENCE"),
+                  selected = "ENGLISH"),
+      sliderInput(inputId = "bins",
+                  label = "Number of Bins",
+                  min = 5,
+                  max = 20,
+                  value = 10)
+    ),
+    mainPanel(
+      plotOutput("distPlot")
     )
+  )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+server <- function(input, output){
+  output$distPlot <- renderPlot({
+    ggplot(exam,
+           aes_string(x = input$variable)) +
+      geom_histogram(bins = input$bins,
+                     color = "black",
+                     fill = "lightblue")
+  })
 }
 
-# Run the application 
 shinyApp(ui = ui, server = server)
